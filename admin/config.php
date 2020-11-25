c80fe5e767f79cf547f8065aa2c7bb9e1a9f6714
navigante <navigante@example.com>
2020-11-25 13:36:52 +0000

    Fuck

--- admin/config.php
+++ admin/config.php
@@ -1,18 +1,18 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8888/artflora/admin/');
-define('HTTP_CATALOG', 'http://localhost:8888/artflora/');
+define('HTTP_SERVER', 'http://178.62.191.143:3003/admin/');
+define('HTTP_CATALOG', 'http://178.62.191.143:3003/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8888/artflora/admin/');
-define('HTTPS_CATALOG', 'http://localhost:8888/artflora/');
+define('HTTPS_SERVER', 'http://178.62.191.143:3003/admin/');
+define('HTTPS_CATALOG', 'http://178.62.191.143:3003/');
 
 // DIR
-define('DIR_APPLICATION', '/Applications/MAMP/htdocs/artflora/admin/');
-define('DIR_SYSTEM', '/Applications/MAMP/htdocs/artflora/system/');
-define('DIR_IMAGE', '/Applications/MAMP/htdocs/artflora/image/');
+define('DIR_APPLICATION', '/opt/artflora-php/artflora/admin/');
+define('DIR_SYSTEM', '/opt/artflora-php/artflora/system/');
+define('DIR_IMAGE', '/opt/artflora-php/artflora/image/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_CATALOG', '/Applications/MAMP/htdocs/artflora/catalog/');
+define('DIR_CATALOG', '/opt/artflora-php/artflora/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -26,9 +26,9 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'artflora');
-define('DB_PASSWORD', 'MYpass24');
-define('DB_DATABASE', 'artflora');
+define('DB_USERNAME', 'artfloraphp');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'artfloraphp');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
